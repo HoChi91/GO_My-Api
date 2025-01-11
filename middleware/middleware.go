1caaa189097e951269972524b07d9ac9c409e2d4
HoChi91 <evan.bazzana@gmail.com>
2025-01-11 19:32:12 +0100

    MOD > nouvelle fonction
    fonction qui verifie si l'user est authentifié

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"MYAPI/helper"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -11,12 +12,12 @@ import (
 
 var JwtKey = []byte(os.Getenv("JWT_SECRET")) // Change cette clé pour une valeur sécurisée
 
+// AuthMiddleware : Vérifie le token, extrait l'utilisateur et vérifie son rôle.
 func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		tokenString, err := c.Cookie("token") // Récupérer le token depuis le cookie
 		if err != nil {
-			helper.HandleError(c, 401, "Token manquant ou invalide",err)
+			helper.HandleError(c, 401, "Token manquant ou invalide", err)
 			c.Abort()
 			return
 		}
@@ -28,31 +29,56 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 			return JwtKey, nil
 		})
 		if err != nil || !token.Valid {
-			helper.HandleError(c, 401, "Token invalide",err)
+			helper.HandleError(c, 401, "Token invalide", err)
 			c.Abort()
 			return
 		}
 
-		if claims, ok := token.Claims.(jwt.MapClaims); ok { // Extraire les claims (données) du token
-			userID := claims["id"].(string)
+		// Extraire les claims (données) du token
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
 			userEmail := claims["email"].(string)
+			userID := claims["id"].(string)
 			role := claims["role"].(string)
 
+			// Stocker les informations dans le contexte de la requête
 			c.Set("userID", userID)
 			c.Set("userEmail", userEmail)
 			c.Set("role", role)
 
 			// Vérifier si le rôle correspond à celui requis
 			if requiredRole != "" && requiredRole != role {
-				helper.HandleError(c, 403, "Accès interdit",err)
+				helper.HandleError(c, 403, "Accès interdit", err)
 				c.Abort()
 				return
 			}
 		} else {
-			helper.HandleError(c, 401, "Token invalide",err)
+			helper.HandleError(c, 401, "Token invalide", err)
 			c.Abort()
 			return
 		}
 		c.Next()
 	}
+}
+
+// CheckAuth : Vérifie si l'utilisateur est authentifié.
+func CheckAuth(c *gin.Context) {
+	tokenString, err := c.Cookie("token")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"isAuthenticated": false})
+		return
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("méthode de signature invalide")
+		}
+		return JwtKey, nil
+	})
+
+	if err != nil || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"isAuthenticated": false})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"isAuthenticated": true})
 }
